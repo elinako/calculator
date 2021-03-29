e676d8115e1bac61d8541b87dc156de015a7e321
Elina <elisonfog@gmail.com>
2021-03-29 22:56:53 +0300

    resolved logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
 function add(s) {
@@ -27,50 +27,86 @@ function devide(s) {
 
 function addMult(s) {
   let arr = [];
+  if ((s.includes("+") && s.includes("-")) || s.includes("-")) {
+    let newArr = [];
+    arr = s.split("-");
 
-  if (s.includes("+")) {
+    newArr = arr.join("+-");
+    const newest = newArr.split("+");
+
+    const arrReadyForMultiple = newest.map((x) => multiple(x));
+
+    const result = arrReadyForMultiple.reduce((acc, value) => value + acc, 0);
+    return result;
+  } else if (s.includes("+")) {
     arr = s.split("+");
+
     return arr.reduce(
       (acc, value) => parseInt(multiple(value)) + parseInt(acc),
       0
     );
-  } else if (s.includes("-")) {
-    arr = s.split("-");
-    const newArr = arr.join("+-");
-    const newest = newArr.split("+");
-    const ddd = newest.map((x) => multiple(x));
+  }
+}
 
-    console.log("arr", ddd);
-    const smth = ddd.reduce((acc, value) => value + acc, 0);
-    console.log("smth", smth);
+function addDevide(s) {
+  if ((s.includes("+") && s.includes("-")) || s.includes("-")) {
+    let newArr = [];
+    const arr = s.split("-");
+    newArr = arr.join("+-");
+
+    const newest = newArr.split("+");
+
+    const arrReadyForMultiple = newest.map((x) => devide(x));
+
+    const result = arrReadyForMultiple.reduce((acc, value) => value + acc, 0);
+    return result;
+  } else if (s.includes("+")) {
+    const arr = s.split("+");
+
+    return arr.reduce((acc, value) => devide(value) + acc, 0);
   }
 }
 
 function App() {
   const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    // const a = value.split("+");
-    // console.log("a", a);
-    // const newA = a.reduce(
-    //   (acc, value) => parseInt(multiple(value)) + parseInt(acc),
-    //   0
-    // );
-
-    // console.log("1222", newA);
-
-    if ((value.includes("+") || value.includes("-")) && value.includes("*")) {
-      console.log("addmult", addMult(value));
-    } else if (value.includes("+") || value.includes("-")) {
-      console.log("add", add(value));
-    } else if (value.includes("*")) {
-      console.log("mult", multiple(value));
-    } else if (value.includes("/")) {
-      console.log("dev", devide(value));
-    }
   };
+
+  useEffect(() => {
+    // setResult(0);
+
+    if (
+      value.slice(-1) === "+" ||
+      value.slice(-1) === "-" ||
+      value.slice(-1) === "*" ||
+      value.slice(-1) === ""
+    ) {
+      return;
+    } else {
+      if ((value.includes("+") || value.includes("-")) && value.includes("*")) {
+        setResult(addMult(value));
+        console.log("admult", addMult(value));
+      } else if (
+        (value.includes("+") || value.includes("-")) &&
+        value.includes("/")
+      ) {
+        console.log(value);
+        setResult(addDevide(value));
+      } else if (value.includes("+") || value.includes("-")) {
+        console.log(value);
+        setResult(add(value));
+      } else if (value.includes("*")) {
+        console.log(value);
+        setResult(multiple(value));
+      } else if (value.includes("/")) {
+        console.log(value);
+        setResult(devide(value));
+      }
+    }
+  }, [value]);
 
   return (
     <div className="App">
@@ -78,8 +114,11 @@ function App() {
         <input
           type="text"
           value={value}
-          onChange={(e) => setValue(e.target.value)}
+          onChange={(event) => {
+            setValue(event.target.value);
+          }}
         />
+        <p>{result}</p>
         <div
           onClick={(e) => setValue(value + e.target.innerText)}
           className={styles.divButton}
